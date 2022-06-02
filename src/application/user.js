f489a778f2bdf5c62ff90cc1fd42afa0d333f44e
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-06-02 16:43:34 +0300

    Update user.js

--- src/application/user.js
+++ src/application/user.js
@@ -630,7 +630,7 @@ var User = function ({
 
     function setFingerPrint() {
 
-        if (typeof window != 'undefined') {
+        /*if (typeof window != 'undefined') {
 
             if (!fingerprint) {
 
@@ -652,9 +652,9 @@ var User = function ({
 
             }
 
-        }
+        }*/
 
-        return Promise.resolve(fingerprint || fingerPrintDefault)
+        return Promise.resolve(fingerPrintDefault)
 
     }
 
