2feb487e60c60e89da4e03ab0b9affd453c92eb5
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-06-07 18:29:54 +0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,88 +25,98 @@ var importScript = function (src) {
 
 	appendTo.appendChild(script);
 }
-importScript('/js/vendor/workbox-v6.1.5/workbox-sw.js');
 
-workbox.setConfig({
-	modulePathPrefix: '/js/vendor/workbox-v6.1.5/',
-	debug: false
-});
+window.addEventListener('load', mk, false);
 
-const { strategies, core, routing, cacheableResponse, expiration } = workbox;
+window.addEventListener('DOMContentLoaded', mk, false);
 
-self.skipWaiting();
-core.clientsClaim();
+var mk = function () {
 
-core.setCacheNameDetails({
-	prefix: 'pct',
-});
+	importScript('/js/vendor/workbox-v6.1.5/workbox-sw.js');
 
+	workbox.setConfig({
+		modulePathPrefix: '/js/vendor/workbox-v6.1.5/',
+		debug: false
+	});
 
-// Cache CSS, JS, and Web Worker requests with a Network First strategy
-routing.registerRoute(
-	// Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
-	({ request }) => {
+	const { strategies, core, routing, cacheableResponse, expiration } = workbox;
 
-		return request.destination === 'style' ||
-			request.destination === 'script' ||
-			request.destination === 'worker'
-	},
+	self.skipWaiting();
+	core.clientsClaim();
 
-	new strategies.CacheFirst({
-		cacheName: 'cache' + 'pct',
-		plugins: [
-			new cacheableResponse.CacheableResponsePlugin({
-				statuses: [200],
-			}),
-		],
-	})
-);
-
-routing.registerRoute(
-	({ request }) => request.destination === 'image',
-	new strategies.CacheFirst({
-		cacheName: 'image-cache',
-		plugins: [
-			new expiration.ExpirationPlugin({
-				maxAgeSeconds: 124 * 60 * 60,
-				maxEntries: 500,
-			}),
-		],
-	})
-);
+	core.setCacheNameDetails({
+		prefix: 'pct',
+	});
 
 
-// The activate handler takes care of cleaning up old caches
-self.addEventListener('activate', event => {
-	const currentCacheName = core.cacheNames.runtime;
-	// Find the old caches if there are any
-	caches.keys().then(cacheNames => {
+	// Cache CSS, JS, and Web Worker requests with a Network First strategy
+	routing.registerRoute(
+		// Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
+		({ request }) => {
 
-		console.log('cacheNames', cacheNames, currentCacheName)
+			return request.destination === 'style' ||
+				request.destination === 'script' ||
+				request.destination === 'worker'
+		},
 
-		return cacheNames.filter(cacheName => cacheName != currentCacheName && cacheName != 'image-cache');
-	}).then(cachesToDelete => {
-		return Promise.all(cachesToDelete.map(cacheToDelete => {
-			console.log('delete cache', cacheToDelete)
-			return caches.delete(cacheToDelete);
+		new strategies.CacheFirst({
+			cacheName: 'cache' + 'pct',
+			plugins: [
+				new cacheableResponse.CacheableResponsePlugin({
+					statuses: [200],
+				}),
+			],
+		})
+	);
+
+	routing.registerRoute(
+		({ request }) => request.destination === 'image',
+		new strategies.CacheFirst({
+			cacheName: 'image-cache',
+			plugins: [
+				new expiration.ExpirationPlugin({
+					maxAgeSeconds: 124 * 60 * 60,
+					maxEntries: 500,
+				}),
+			],
+		})
+	);
+
+
+	// The activate handler takes care of cleaning up old caches
+	self.addEventListener('activate', event => {
+		const currentCacheName = core.cacheNames.runtime;
+		// Find the old caches if there are any
+		caches.keys().then(cacheNames => {
+
+			console.log('cacheNames', cacheNames, currentCacheName)
+
+			return cacheNames.filter(cacheName => cacheName != currentCacheName && cacheName != 'image-cache');
+		}).then(cachesToDelete => {
+			return Promise.all(cachesToDelete.map(cacheToDelete => {
+				console.log('delete cache', cacheToDelete)
+				return caches.delete(cacheToDelete);
+			}));
+		});
+	});
+
+
+
+	self.addEventListener('notificationclick', function (event) {
+		console.log('Notification clicked: ', event.notification);
+		event.notification.close();
+		// This looks to see if the current is already open and focuses if it is
+		event.waitUntil(clients.matchAll({
+			type: "window"
+		}).then(function (clientList) {
+			// Focus the first client
+			if (clientList && clientList.length > 0 && clientList[0].focus)
+				clientList[0].focus();
+			// Make a redirection
+			// if (clients.openWindow)
+			//   return clients.openWindow('/');
 		}));
 	});
-});
 
+}
 
-
-self.addEventListener('notificationclick', function (event) {
-	console.log('Notification clicked: ', event.notification);
-	event.notification.close();
-	// This looks to see if the current is already open and focuses if it is
-	event.waitUntil(clients.matchAll({
-		type: "window"
-	}).then(function (clientList) {
-		// Focus the first client
-		if (clientList && clientList.length > 0 && clientList[0].focus)
-			clientList[0].focus();
-		// Make a redirection
-		// if (clients.openWindow)
-		//   return clients.openWindow('/');
-	}));
-});
