d61a0f7a0874431dd25ec01bb5e5e61a73d744e2
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-07-14 16:51:42 +0300

    v

--- src/service-worker.js
+++ src/service-worker.js
@@ -15,7 +15,7 @@ core.clientsClaim();
 
 core.setCacheNameDetails({
 	prefix: 'pct',
-	suffix: '0.0.15'
+	suffix: '0.0.16'
 });
 
 // Cache CSS, JS, and Web Worker requests with a Network First strategy
