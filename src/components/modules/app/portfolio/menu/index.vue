7bb0debcecdc6a2cfb9fddb7f25de689b0a350ed
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2022-08-24 13:17:49 +0300

    v

--- src/components/modules/app/portfolio/menu/index.vue
+++ src/components/modules/app/portfolio/menu/index.vue
@@ -156,8 +156,8 @@ export default {
 
 			this.$dialog.confirm(
 				"Do you really want to delete this portfolio?", {
-				okText: vm.$t('yes'),
-				cancelText : vm.$t('no')
+				okText: this.$t('yes'),
+				cancelText : this.$t('no')
 			})
 	
 			.then((dialog) => {
