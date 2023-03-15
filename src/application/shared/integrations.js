089b00b5a2e2523f96f197c7284d328ef2fa5121
Alexander <zanhesl@gmail.com>
2023-03-15 16:59:00 +0300

    Added integration fields

--- src/application/shared/integrations.js
+++ src/application/shared/integrations.js
@@ -1,40 +1,473 @@
-class Integrations {
-    constructor() {}
+const STATIC_INTEGRATIONS = [
+    {
+        value: 'AMERITRADE',
+        text: "TD Ameritrade Institutional's Veo®",
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Password',
+                text: 'fields.password',
+                type: 'password',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        message: 'fields.strongpassword',
+                        rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'RepCode',
+                text: 'fields.RepCode',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'MORNINGSTAR',
+        text: 'Morningstar Office®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'AccessCode',
+                text: 'fields.AccessCode',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'ORION',
+        text: 'Orion®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Password',
+                text: 'fields.password',
+                type: 'password',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        message: 'fields.strongpassword',
+                        rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+                    },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'BLACKDIAMOND',
+        text: 'Black Diamond®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Password',
+                text: 'fields.password',
+                type: 'password',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        message: 'fields.strongpassword',
+                        rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+                    },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'STERNEAGEE',
+        text: 'SA Stone®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Password',
+                text: 'fields.password',
+                type: 'password',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        message: 'fields.strongpassword',
+                        rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'RepCode',
+                text: 'fields.RepCode',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'REDTAIL',
+        text: 'Redtail®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Password',
+                text: 'fields.password',
+                type: 'password',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        message: 'fields.strongpassword',
+                        rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+                    },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'ALBRIDGE',
+        text: 'Albridge®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Password',
+                text: 'fields.password',
+                type: 'password',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        message: 'fields.strongpassword',
+                        rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+                    },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'RepCode',
+                text: 'fields.RepCode',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+    {
+        value: 'SCHWAB',
+        text: 'Charles Schwab Corporation®',
+        fields: [
+            {
+                id: 'IntegrationName',
+                text: 'fields.IntegrationName',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    // {
+                    //     rule: 'email',
+                    // },
+                    // {
+                    //     rule: 'min:5',
+                    // },
+                ],
+                input: 'input',
+            },
+            {
+                id: 'Login',
+                text: 'fields.Login',
+                rules: [
+                    {
+                        rule: 'required',
+                    },
+                    {
+                        rule: 'email',
+                    },
+                    {
+                        rule: 'min:5',
+                    },
+                ],
+                input: 'input',
+            },
+        ],
+    },
+];
 
-    staticIntegrations = [
-        {
-            value: 'AMERITRADE',
-            text: "TD Ameritrade Institutional's Veo®",
-        },
-        {
-            value: 'MORNINGSTAR',
-            text: 'Morningstar Office®',
-        },
-        {
-            value: 'ORION',
-            text: 'Orion®',
-        },
-        {
-            value: 'BLACKDIAMOND',
-            text: 'Black Diamond®',
-        },
-        {
-            value: 'STERNEAGEE',
-            text: 'SA Stone®',
-        },
-        {
-            value: 'REDTAIL',
-            text: 'Redtail®',
-        },
-        {
-            value: 'ALBRIDGE',
-            text: 'Albridge®',
-        },
-        {
-            value: 'SCHWAB',
-            text: 'Charles Schwab Corporation®',
-        },
-    ];
+class SingleIntegration {
+    constructor(staticIntegration) {
+        const { value, text, fields } = staticIntegration;
+        this.value = value;
+        this.text = text;
+        this.fields = fields;
+    }
+}
+
+class Integrations {
+    constructor() {
+        this.staticIntegrations = STATIC_INTEGRATIONS.map(
+            (integration) => new SingleIntegration(integration),
+        );
+    }
 }
 
 export default Integrations;
--- src/components/modules/app/settings/integrations/add/index.js
+++ src/components/modules/app/settings/integrations/add/index.js
@@ -9,6 +9,7 @@ export default {
             loading: false,
             type: null,
             values: {},
+            fields: null,
         };
     },
 
@@ -34,115 +35,122 @@ export default {
             ];
         },
 
-        fields: function () {
-            if (this.type == 'none2')
-                return [
-                    {
-                        id: 'Email',
-                        text: 'fields.Email',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                            {
-                                rule: 'email',
-                            },
-                            {
-                                rule: 'min:5',
-                            },
-                        ],
-                    },
+        // fields: function () {
+        //     return this.fields;
+        //     if (this.type == 'none2')
+        //         return [
+        //             {
+        //                 id: 'Email',
+        //                 text: 'fields.Email',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                     {
+        //                         rule: 'email',
+        //                     },
+        //                     {
+        //                         rule: 'min:5',
+        //                     },
+        //                 ],
+        //             },
 
-                    {
-                        id: 'FirstName',
-                        text: 'fields.FName',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'LastName',
-                        text: 'fields.LName',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'CompanyName',
-                        text: 'fields.Company',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'Title',
-                        text: 'fields.Title',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'Phone',
-                        text: 'fields.Phone',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                            {
-                                rule: 'phone',
-                            },
-                            {
-                                rule: 'min:5',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'MobilePhone',
-                        text: 'fields.mobilephone',
-                    },
+        //             {
+        //                 id: 'FirstName',
+        //                 text: 'fields.FName',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                 ],
+        //             },
+        //             {
+        //                 id: 'LastName',
+        //                 text: 'fields.LName',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                 ],
+        //             },
+        //             {
+        //                 id: 'CompanyName',
+        //                 text: 'fields.Company',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                 ],
+        //             },
+        //             {
+        //                 id: 'Title',
+        //                 text: 'fields.Title',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                 ],
+        //             },
+        //             {
+        //                 id: 'Phone',
+        //                 text: 'fields.Phone',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                     {
+        //                         rule: 'phone',
+        //                     },
+        //                     {
+        //                         rule: 'min:5',
+        //                     },
+        //                 ],
+        //             },
+        //             {
+        //                 id: 'MobilePhone',
+        //                 text: 'fields.mobilephone',
+        //             },
 
-                    {
-                        id: 'Password',
-                        text: 'fields.password',
-                        type: 'password',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                            {
-                                message: 'fields.strongpassword',
-                                rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
-                            },
-                        ],
-                    },
-                    {
-                        id: 'Confirm password field',
-                        text: 'fields.confirmpassword',
-                        type: 'password',
-                        rules: [
-                            {
-                                rule: 'required',
-                            },
-                            {
-                                rule: 'same:Password',
-                            },
-                        ],
-                    },
-                ];
-        },
+        //             {
+        //                 id: 'Password',
+        //                 text: 'fields.password',
+        //                 type: 'password',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                     {
+        //                         message: 'fields.strongpassword',
+        //                         rule: 'regex:^(?=.*[!@#$%^&*-])(?=.*[0-9])(?=.*[A-Z]).{8,20}$',
+        //                     },
+        //                 ],
+        //             },
+        //             {
+        //                 id: 'Confirm password field',
+        //                 text: 'fields.confirmpassword',
+        //                 type: 'password',
+        //                 rules: [
+        //                     {
+        //                         rule: 'required',
+        //                     },
+        //                     {
+        //                         rule: 'same:Password',
+        //                     },
+        //                 ],
+        //             },
+        //         ];
+        // },
     }),
 
-        inputType : function(v){
-            this.type = v.type
-            this.values = {}
+    methods: {
+        input: function (v) {},
+
+        inputType: function (v) {
+            this.type = v.type;
+            this.values = {};
+            this.fields = this.core.integrations.staticIntegrations.find(
+                (int) => int.value === v.type,
+            ).fields;
         },
 
         save: function () {},
