cb50a737561d85a6c64e133b973dce62b41c2014
maxgithubprofile <21055122+maxgithubprofile@users.noreply.github.com>
2023-06-09 17:30:40 +0300

    v

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
-    '@babel/preset-env'
+    '@babel/preset-env',
+    "mjs"
   ],
   plugins: [
     '@babel/plugin-transform-typescript',
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-async-to-generator',
     '@babel/plugin-proposal-async-generator-functions',
-    '@babel/plugin-syntax-async-generators'
+    '@babel/plugin-syntax-async-generators',
+    '@babel/plugin-transform-nullish-coalescing-operator'
   ]
 }
